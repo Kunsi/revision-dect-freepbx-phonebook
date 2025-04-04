9064e5259d127ed0a0de37218c130e7889ed97bd
Franziska Kunsmann <hi@kunsmann.eu>
2025-04-04 21:58:36 +0200

    try to fix weird behaviour where freepbx extensions won't show up

--- phonebook/__init__.py
+++ phonebook/__init__.py
@@ -1,13 +1,11 @@
 from json import load as json_load
+from os import environ
 from os.path import abspath, dirname, join
 
+import mariadb
 from flask import Flask, jsonify, render_template, request
 
-from .freepbx import FreePBXPhonebook
-
 app = Flask("phonebook")
-
-fpbx = FreePBXPhonebook()
 
 
 def _omm_pp_list():
@@ -15,12 +13,38 @@ def _omm_pp_list():
         return json_load(f)
 
 
+def _freepbx_phonebook():
+    db = mariadb.connect(
+        user=environ["FREEPBX_DB_USER"],
+        password=environ["FREEPBX_DB_PASS"],
+        host=environ["FREEPBX_DB_HOST"],
+        port=3306,
+        database="asterisk",
+    )
+
+    result = {}
+
+    # get actual extensions
+    with db.cursor() as cur:
+        cur.execute("SELECT extension, name FROM users;")
+        for ext, name in cur:
+            result[str(ext)] = name
+
+    # get group calls (aka "ring groups")
+    with db.cursor() as cur:
+        cur.execute("SELECT grpnum, description FROM ringgroups;")
+        for ext, name in cur:
+            result[str(ext)] = name
+
+    return result
+
+
 @app.route("/")
 def phonebook_html():
     return render_template(
         "phonebook.html",
         force_display_all=bool("all" in request.args),
-        fpbx=fpbx.phonebook(),
+        fpbx=_freepbx_phonebook(),
         omm=_omm_pp_list(),
     )
 
@@ -29,14 +53,14 @@ def phonebook_html():
 def phonebook_xml():
     return render_template(
         "phonebook.xml",
-        fpbx=fpbx.phonebook(),
+        fpbx=_freepbx_phonebook(),
         omm=_omm_pp_list(),
     )
 
 
 @app.route("/freepbx.json")
 def phonebook_freepbx():
-    return jsonify(fpbx.phonebook())
+    return jsonify(_freepbx_phonebook())
 
 
 @app.route("/omm.json")

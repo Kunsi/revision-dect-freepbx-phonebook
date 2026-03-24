ab26f9081d9cd12b839ce0ed7535e413dae7d9fd
Franziska Kunsmann <hi@kunsmann.eu>
2026-03-24 16:56:23 +0100

    rework phonebook to be easier to read

--- phonebook/__init__.py
+++ phonebook/__init__.py
@@ -2,12 +2,43 @@ from json import load as json_load
 from os.path import abspath, dirname, join
 from xml.etree import ElementTree
 
-
 import qrcode
 import qrcode.image.svg
-from flask import Flask, jsonify, render_template, request, Response
+from flask import Flask, Response, abort, jsonify, render_template, request
 
 app = Flask("phonebook")
+
+
+def _phonebook_as_list(force_display_all=False):
+    fpbx = freepbx_phonebook()
+    omm = omm_pp_list()
+
+    result = []
+
+    for number, name in sorted(fpbx.items()):
+        should_be_shown = force_display_all
+        is_active = True
+        omm_info = omm.get(number, {})
+
+        if not omm_info:
+            should_be_shown = True
+
+        if omm_info.get("is_subscribed", True):
+            should_be_shown = True
+
+        if omm_info.get("is_active", True) is False:
+            is_active = False
+
+        result.append(
+            {
+                "number": number,
+                "name": name,
+                "is_active": is_active,
+                "should_be_shown": should_be_shown,
+            }
+        )
+
+    return result
 
 
 def omm_pp_list():
@@ -25,18 +56,22 @@ def make_qrcode():
     if not request.args.get("url"):
         abort(404)
 
-    image = qrcode.make(request.args['url'], image_factory=qrcode.image.svg.SvgImage)
+    image = qrcode.make(request.args["url"], image_factory=qrcode.image.svg.SvgImage)
     svg_data = ElementTree.tostring(image.get_image()).decode()
     return Response(svg_data, content_type="image/svg+xml")
 
 
 @app.route("/")
 def phonebook_html():
+    force_display_all = bool("all" in request.args) or request.args.get("qr")
+    phonebook = _phonebook_as_list(force_display_all=force_display_all)
+
+    has_hidden_numbers = not all([i["should_be_shown"] for i in phonebook])
+
     return render_template(
         "phonebook.html",
-        force_display_all=bool("all" in request.args),
-        fpbx=freepbx_phonebook(),
-        omm=omm_pp_list(),
+        phonebook=phonebook,
+        has_hidden_numbers=has_hidden_numbers,
         qr=request.args.get("qr"),
     )
 
@@ -45,8 +80,7 @@ def phonebook_html():
 def phonebook_xml():
     return render_template(
         "phonebook.xml",
-        fpbx=freepbx_phonebook(),
-        omm=omm_pp_list(),
+        phonebook=_phonebook_as_list(force_display_all=True),
     )
 
 
